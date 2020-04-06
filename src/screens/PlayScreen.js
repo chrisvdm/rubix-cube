98a34e215227061d4ed611ac80929c09bcdf0a77
chrisvdm <nymanchristine@gmail.com>
2020-04-06 23:11:21 +0200

    horizontal rotate view functionality

--- src/screens/PlayScreen.js
+++ src/screens/PlayScreen.js
@@ -14,11 +14,30 @@ class PlayScreen extends Component {
   }
 
   onRotateLeftBtnClick () {
+    // -1
     console.log('left')
+    const vs = this.state.visibleSides
+    const hb = vs.slice(1, 3)
+    const newb = hb.map(b => (b - 1 < 1 ? 4 : b - 1))
+
+    const foo = vs.slice(0, 1)
+    const newVS = foo.concat(newb)
+    this.setState({ visibleSides: newVS })
   }
 
   onRotateRightBtnClick () {
+    // +1
     console.log('right')
+    const vs = this.state.visibleSides
+    const hb = vs.slice(1, 3)
+    const newb = hb.map(b => (b + 1 > 4 ? 1 : b + 1))
+    const foo = vs.slice(0, 1)
+    const newVS = foo.concat(newb)
+    this.setState({ visibleSides: newVS })
+    // [0,1,2]
+    // [0,2,3]
+    // [0,3,4]
+    // [0,4,1]
   }
 
   render () {
