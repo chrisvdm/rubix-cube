52cf6b4315cc5f69a0fbdbe1a215f82b06a7354a
chrisvdm <nymanchristine@gmail.com>
2020-04-10 21:29:50 +0200

    Add rotation func

--- src/components/RubixCube/RubixCubeWrapper.js
+++ src/components/RubixCube/RubixCubeWrapper.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const RubixCubeWrapper = React.forwardRef((props, ref) => (
+const RubixCubeWrapper = React.forwardRef(({ children }, ref) => (
   <div className='rubic-cube__scene'>
     <div className='rubix-cube--wrapper' ref={ref}>
-      {props.children}
+      {children}
     </div>
   </div>
 ))
--- src/screens/PlayScreen.js
+++ src/screens/PlayScreen.js
@@ -6,46 +6,56 @@ class PlayScreen extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      visibleSides: [0, 1, 2]
+      visibleSides: [0, 1, 2],
+      rotation: { y: 45, x: -15 }
     }
 
     this.onRotateLeftBtnClick = this.onRotateLeftBtnClick.bind(this)
     this.onRotateRightBtnClick = this.onRotateRightBtnClick.bind(this)
+    this.onRotateUpBtnClick = this.onRotateUpBtnClick.bind(this)
     this.cube = React.createRef()
   }
 
+  newRotation (dir, rotation, view) {
+    if (dir !== 0) {
+      const index = view.findIndex(deg => deg === rotation) + dir
+      if (index === view.length) {
+        return view[0]
+      } else if (index < 0) {
+        return view[view.length - 1]
+      } else {
+        return view[index]
+      }
+    }
+    return rotation
+  }
+
+  rotateCube (h, v) {
+    const {
+      rotation: { y, x }
+    } = this.state
+    const hView = [45, 135, 225, 315]
+    const vView = [-15, 15]
+    const hValue = this.newRotation(h, y, hView)
+    const vValue = this.newRotation(v, x, vView)
+    const template = `translateZ(-400px) rotateY(${hValue}deg) rotateX(${vValue}deg) rotateZ(${vValue}deg)`
+
+    this.setState(
+      { rotation: { y: hValue, x: vValue } },
+      () => (this.cube.current.style.transform = template)
+    )
+  }
+
+  onRotateUpBtnClick () {
+    this.rotateCube(0, 1)
+  }
+
   onRotateLeftBtnClick () {
-    const rotations = [
-      'translateZ(-150px) rotateY(90deg)',
-      'translateZ(-150px) rotateY(180deg)',
-      'translateZ(-150px) rotateY(270deg)',
-      'translateZ(-150px) rotateY(0deg)'
-    ]
-
-    const inline = this.cube.current.style.transform
-    const i = rotations.findIndex(r => {
-      return r === inline
-    })
-    const next = i === 3 ? 0 : i + 1
-
-    this.cube.current.style.transform = rotations[next]
+    this.rotateCube(1, 0)
   }
 
   onRotateRightBtnClick () {
-    const rotations = [
-      'translateZ(-150px) rotateY(-90deg)',
-      'translateZ(-150px) rotateY(180deg)',
-      'translateZ(-150px) rotateY(90deg)',
-      'translateZ(-150px) rotateY(0deg)'
-    ]
-
-    const inline = this.cube.current.style.transform
-    const i = rotations.findIndex(r => {
-      return r === inline
-    })
-    const next = i === 3 ? 0 : i + 1
-
-    this.cube.current.style.transform = rotations[next]
+    this.rotateCube(-1, 0)
   }
 
   render () {
